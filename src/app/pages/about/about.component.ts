fb4f7a14f5a3454f4cfab88fb93dbd3beaeb6e3c
george yaguana <sebyaguana@gmail.com>
2023-08-15 18:25:23 -0500

    proyecto Guia 19

---
+++ src/app/pages/about/about.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent {
+  description: string = 'Dewscripcion del proyectyo Final Dawm'; /* Cambie el texto con la descripción de su proyecto */
+}
