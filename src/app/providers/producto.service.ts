d260c9f489f12ecba337a18088379b273a6c6a68
george yaguana <sebyaguana@gmail.com>
2023-09-01 20:46:56 -0500

    actualizacion

--- src/app/providers/producto.service.ts
+++ src/app/providers/producto.service.ts
@@ -5,7 +5,7 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class ProveedorProductoService {
-  private URL: string = '  https://rest-landing-production.up.railway.app/rest/producto/findAll/json';
+  private URL: string = 'https://rest-landing-production.up.railway.app/rest/producto/findAll/json';
 
   getResponse() {
     return this.http.get(this.URL);
